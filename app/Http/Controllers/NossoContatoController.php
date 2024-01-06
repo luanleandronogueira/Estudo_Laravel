5f1d4842bdf3d6823de01663728d078be5fbd191
luanleandronogueira <luannogueira093@gmail.com>
2024-01-06 10:46:48 -0300

    firts commit

---
+++ app/Http/Controllers/NossoContatoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NossoContatoController extends Controller
+{
+    public function Contato() {
+
+        return view('contato');
+        
+    }
+}
---
+++ resources/views/sobrenos.blade.php
@@ -0,0 +1,10 @@
+<h3>Sobre Nós (view)</h3>
+
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}">Principal</a></li>
+    <li>
+        <a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+    <li>
+        <a href="{{ route('site.contato') }}">Contato</a></li>
+</ul>
